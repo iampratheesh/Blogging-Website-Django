5820609d11c86447adda6ce9388aea4e73d0a861
Pratheesh Shivaprasad <pratheesh.27998@gmail.com>
2022-05-03 11:55:30 +0530

    Initial User Profile Page Created

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -32,5 +36,8 @@ urlpatterns = [
         auth_views.LogoutView.as_view(template_name="users/logout.html"),
         name="logout",
     ),
-    path("profile/", user_views.profile, name="profile")
+    path("profile/", user_views.profile, name="profile"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
